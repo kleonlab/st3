2b3ebd89aa6e992224ea0e69ace8181e9043ab68
kleonlab <kleoninfo@gmail.com>
2026-01-22 13:24:49 +0000

    local pt model run

--- staticscripts/check_emb.py
+++ staticscripts/check_emb.py
@@ -1,8 +1,8 @@
 
 import torch
 
-pt_path = "/home/b5cc/sanjukta.b5cc/st3/datasets/5k/perturbation_mapping_5k.pt"
-test_pt_list_path = "/home/b5cc/sanjukta.b5cc/st3/datasets/5k/perturbation_names_5k.txt"
+pt_path = "/home/b5cc/sanjukta.b5cc/st3/datasets/30k/protein_embeddings.pt"
+test_pt_list_path = "/home/b5cc/sanjukta.b5cc/st3/datasets/30k/perturbation_names_all.txt"
 
 
 def check_perturbation(pt_path, test_pt_list_path):
