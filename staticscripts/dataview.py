92796b402a76ad37ca89f2ed2590210d9af4e0c7
kleonlab <kleoninfo@gmail.com>
2026-01-25 17:34:33 +0000

    local runs on inf larger samples steps

--- staticscripts/dataview.py
+++ staticscripts/dataview.py
@@ -1,7 +1,7 @@
 import scanpy as sc
 import os
 #adata = sc.read_h5ad("../datasets/competition_support_set/k562_gwps.h5")
-adata = sc.read_h5ad("/home/b5cc/sanjukta.b5cc/st3/datasets/hepg2/hepg2.h5ad")
+adata = sc.read_h5ad("/home/b5cc/sanjukta.b5cc/st3/datasets/30k/k562_filtered.h5ad")
 
 print(adata)
 print(len(adata))
@@ -11,6 +11,7 @@ print(adata.X[0])
 print(adata.X[2])
 
 print(adata.obs['gene'].value_counts())
+print(adata.obs['cell_type'].value_counts())
 # Save as a simple text file with two columns
 #counts = adata.obs['gene'].value_counts()
 #output_dir = "/home/b5cc/sanjukta.b5cc/st3/datasets/k562"
